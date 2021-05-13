e7df7c6c7ea8e4d400e3dcb2eec31294c56d8702
bastiman85 <github@bastiman.nu>
2021-05-14 00:17:51 +0200

    Initial commit

---
+++ README-template.md
@@ -0,0 +1,62 @@
+# Frontend Mentor - Crowdfunding product page solution
+
+This is a solution to the [Crowdfunding product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/crowdfunding-product-page-7uvcZe7ZR). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- Make a selection of which pledge to make
+- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
+- See the number of total backers increment by one after confirming a pledge
+- Toggle whether or not the product is bookmarked
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Javascript (Vanilla)
+
+### What I learned
+
+I learned a lot about replacing text content and numbers in the correct format through Javascript.
+I also got to experiment a bit about changing colors of SVGs.
+
+### Continued development
+
+I need to focus on writing more DRY JS. In additions I need to comment more in my code and also structure it better.
+
+## Author
+
+- My work - [portfolio.bastiman.nu](https://portfolio.bastiman.nu)
+- Frontend Mentor - [@bastiman85](https://www.frontendmentor.io/profile/bastiman85)
---
+++ js/main.js
@@ -0,0 +1,185 @@
+const modalContainer = document.querySelector(".modal-container");
+var buttons = document.querySelectorAll("main .btn-primary");
+var radios = document.querySelectorAll(".modal input[type='radio']");
+var close = document.querySelector(".close-modal");
+var modalPledge = document.querySelector(".modal-pledge");
+var modalSuccess = document.querySelector(".modal-success");
+var closeSuccess = document.querySelector(".modal-success button");
+var mobileMenu = document.querySelector(".mobile-menu");
+var amountBacked = 89914;
+var backers = 5007;
+
+for (i = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener("click", openModal);
+}
+
+function openModal(e) {
+   if (e.target.id == "no-reward-btn") {
+      var radiobtns = document.querySelectorAll(".card input[type='radio']");
+      for (i = 0; i < radiobtns.length; i++) {
+         radiobtns[i].checked = false;
+      }
+      var pledgebox = document.getElementById("no-reward-card");
+      clearPledgeBoxes();
+      // pledgebox.classList.toggle("selected");
+   }
+   if (e.target.id == "bamboo-reward-btn") {
+      var radiobtn = document.getElementById("bamboo-reward");
+      radiobtn.checked = true;
+      var pledgebox = document.getElementById("bamboo-reward-card");
+      clearPledgeBoxes();
+      pledgebox.classList.toggle("selected");
+   }
+   if (e.target.id == "blackedition-reward-btn") {
+      var radiobtn = document.getElementById("blackedition-reward");
+      radiobtn.checked = true;
+      var pledgebox = document.getElementById("blackedition-reward-card");
+      clearPledgeBoxes();
+      pledgebox.classList.toggle("selected");
+   }
+
+   modalContainer.style.display = "grid";
+   modalPledge.style.display = "block";
+}
+
+for (i = 0; i < radios.length; i++) {
+   radios[i].addEventListener("change", openPledge);
+}
+
+function openPledge(e) {
+   if (e.target.id == "no-reward") {
+      var pledgebox = document.getElementById("no-reward-card");
+      clearPledgeBoxes();
+      pledgebox.classList.toggle("selected");
+   }
+   if (e.target.id == "bamboo-reward") {
+      var pledgebox = document.getElementById("bamboo-reward-card");
+      clearPledgeBoxes();
+      pledgebox.classList.toggle("selected");
+   }
+   if (e.target.id == "blackedition-reward") {
+      var pledgebox = document.getElementById("blackedition-reward-card");
+      clearPledgeBoxes();
+      pledgebox.classList.toggle("selected");
+   }
+}
+
+function clearPledgeBoxes() {
+   var pledgeBoxes = document.querySelectorAll(".card.selected");
+   for (i = 0; i < pledgeBoxes.length; i++) {
+      pledgeBoxes[i].classList.remove("selected");
+   }
+}
+
+/* Closes the modal when X is clicked */
+close.addEventListener("click", closeModal);
+
+function closeModal() {
+   modalContainer.style.display = "none";
+}
+
+var pledgeSubmitButtons = document.querySelectorAll(".pledge button");
+
+for (i = 0; i < pledgeSubmitButtons.length; i++) {
+   pledgeSubmitButtons[i].addEventListener("click", submitPledge);
+}
+
+function submitPledge(e) {
+   if (validateForm(true)) {
+      var rewardsLeft = document.querySelectorAll(".rewards .amount-left span");
+      if (e.target.id == "pledge1submit") {
+         var addedAmount = e.target.previousElementSibling.childNodes[1].value;
+         backers++;
+         updateStats(addedAmount, backers);
+      }
+      if (e.target.id == "pledge2submit") {
+         var addedAmount = e.target.previousElementSibling.childNodes[1].value;
+         backers++;
+         rewardsLeft[0].innerHTML = Number(rewardsLeft[0].innerHTML) - 1;
+         rewardsLeft[3].innerHTML = Number(rewardsLeft[3].innerHTML) - 1;
+         updateStats(addedAmount, backers);
+      }
+      if (e.target.id == "pledge3submit") {
+         var addedAmount = e.target.previousElementSibling.childNodes[1].value;
+         backers++;
+         rewardsLeft[1].innerHTML = Number(rewardsLeft[1].innerHTML) - 1;
+         rewardsLeft[4].innerHTML = Number(rewardsLeft[4].innerHTML) - 1;
+         updateStats(addedAmount, backers);
+      }
+
+      modalPledge.style.display = "none";
+      modalSuccess.style.display = "block";
+   }
+}
+
+/* Updates becked amount, backers and progress bar */
+function updateStats(amount, backers) {
+   amountBacked += Number(amount);
+   document.querySelector(".amount .number span").innerHTML = amountBacked.toLocaleString("en-US");
+   if (amountBacked <= 100000) {
+      document.querySelector(".progress").style.width = "calc(" + String(amountBacked) + " / 100000 * 100%)";
+   } else {
+      document.querySelector(".progress").style.width = "100%";
+   }
+   document.querySelector(".backers .number").innerHTML = backers.toLocaleString("en-US");
+}
+
+/* Closes modal on pledge submit */
+closeSuccess.addEventListener("click", function () {
+   modalSuccess.style.display = "none";
+   closeModal();
+});
+
+/* Handles the hamburger menu on mobile */
+mobileMenu.addEventListener("click", openMenu);
+
+function openMenu() {
+   document.querySelector(".nav-list").classList.add("open");
+   document.body.classList.add("overlay");
+   mobileMenu.removeEventListener("click", openMenu);
+   mobileMenu.addEventListener("click", closeMenu);
+   console.log(mobileMenu);
+   mobileMenu.childNodes[0].src = "./images/icon-close-menu.svg";
+}
+
+function closeMenu() {
+   document.querySelector(".nav-list").classList.remove("open");
+   document.body.classList.remove("overlay");
+   mobileMenu.removeEventListener("click", closeMenu);
+   mobileMenu.addEventListener("click", openMenu);
+   mobileMenu.childNodes[0].src = "./images/icon-hamburger.svg";
+}
+
+// var modalh3 = document.querySelectorAll(".modal h3");
+
+// for (i = 0; i < modalh3.length; i++) {
+//    modalh3[i].addEventListener("mouseover", function (event) {
+//       event.target.style.color = "red";
+//    });
+//    modalh3[i].addEventListener("mouseout", function (event) {
+//       event.target.style.color = "var(--clr-neutral-dark)";
+//    });
+// }
+
+// function mouseOver(h3) {
+//    h3.style.color = "red";
+// }
+
+// function mouseOut(h3) {
+//    // h3.style.color = "var(--clr-neutral-dark)";
+// }
+
+function validateForm() {
+   var pledge2valid = document.getElementById("pledge2").value;
+   var pledge3valid = document.getElementById("pledge3").value;
+   if (pledge2valid == null || pledge2valid < 25) {
+      alert("Pledge must be $25 or higher");
+      return false;
+   }
+   if (pledge3valid == null || pledge3valid < 75) {
+      alert("Pledge must be $75 or higher");
+      return false;
+   } else {
+      return true;
+   }
+}
